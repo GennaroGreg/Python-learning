1d6906f9d8757c55d0f9857a89d0e8ac212dd2f1
GennaroGreg <gennarogreg@gmail.com>
2024-06-21 15:46:17 -0500

    Lessons 1 & 2

---
+++ Lesson 2/welcome.py
@@ -0,0 +1,10 @@
+line01 = "********************" # header / footer
+line02 = "*                  *" # blank space
+line03 = "*     Welcome!     *" # Message
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
