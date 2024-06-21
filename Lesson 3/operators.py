e0eb90b42a8569f2ba73d13c4aa9f35f99120f9b
GennaroGreg <gennarogreg@gmail.com>
2024-06-21 16:44:23 -0500

    Lessons 3 & 4

---
+++ Lesson 3/operators.py
@@ -0,0 +1,11 @@
+value = 20
+print('')
+
+# Multi-line if/else
+if value > 10:
+    print('Hooray :)')
+else:
+    print('Boo hoo :(')
+
+# Ternary Operator
+print('YES!') if value > 10 else print('NO!')
